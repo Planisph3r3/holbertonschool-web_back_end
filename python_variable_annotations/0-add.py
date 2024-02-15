17acd36e66cc30bb36b5de20cd8be8a1b430ad40
Planisph3r3 <ennzopz@gmail.com>
2024-02-15 17:51:10 -0500

    Basic annotations

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Functions with simple variable annotations"""
+
+
+def add(a: float, b: float) -> float:
+    """Function that sums a pair of floats and returns one"""
+    return float(a + b)
