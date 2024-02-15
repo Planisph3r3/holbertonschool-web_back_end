d56ea4c70bfdb466a71c001615b91b31563a0e5c
Planisph3r3 <ennzopz@gmail.com>
2024-02-15 17:54:53 -0500

    Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Functions with simple variable annotations"""
+
+
+def to_str(n: float) -> str:
+    """Function that returns the string repr of a float"""
+    return str(n)
