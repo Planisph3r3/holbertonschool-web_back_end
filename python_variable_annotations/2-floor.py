ea6beb944d7d10552bf0e35b203083776885b942
Planisph3r3 <ennzopz@gmail.com>
2024-02-15 17:54:44 -0500

    Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Functions with simple variable annotations"""
+
+
+def concat(n: float) -> int:
+    """Function that rounds down a float and returns an int"""
+    return int(n // 1)
