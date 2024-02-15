86b064dbdd9e43a04b01aa0f4a7e2148eaa169ae
Planisph3r3 <ennzopz@gmail.com>
2024-02-15 17:54:12 -0500

    Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Functions with simple variable annotations"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function that concatenates two strings"""
+    return str(str1 + str2)
