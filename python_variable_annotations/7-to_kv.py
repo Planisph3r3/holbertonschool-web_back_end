b5fc9983f8e6a73a5cc546c91d83e269cb12efc2
Planisph3r3 <ennzopz@gmail.com>
2024-02-15 19:24:20 -0500

    Correct importing Tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Function that that takes a string k and an int OR float v
     as arguments and returns a tuple.
