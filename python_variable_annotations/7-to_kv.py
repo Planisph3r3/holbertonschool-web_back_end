19f727ef06473a2e45353e759cf847063b5b8144
Planisph3r3 <ennzopz@gmail.com>
2024-02-15 19:31:04 -0500

    Fix: Return value not casted into tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -10,4 +10,4 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     Function that that takes a string k and an int OR float v
     as arguments and returns a tuple.
     """
-    return tuple(k, v ** 2)
+    return (k, v ** 2)
