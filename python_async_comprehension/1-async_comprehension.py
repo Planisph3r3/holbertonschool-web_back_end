f9b0e87db1a9af418c5d4a4558a9e04db4c1f441
Planisph3r3 <ennzopz@gmail.com>
2024-02-25 16:01:15 -0500

    Corrected code

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -12,7 +12,8 @@ async_generator = __import__("0-async_generator").async_generator
 
 async def async_comprehension() -> List[float]:
     """
-    Coroutine that collects 10 random numbers using async comprehension over async_generator,
+    Coroutine that collects 10 random numbers using async
+    comprehension over async_generator,
     then returns the 10 random numbers.
     """
     return [val async for val in async_generator()]
