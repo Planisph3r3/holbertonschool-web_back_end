daedd79815a44ff4efd0067d812faf2420f98da4
Planisph3r3 <ennzopz@gmail.com>
2024-02-25 15:32:03 -0500

    Corrected requierements

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -9,6 +9,12 @@ import typing
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """
+    An async coroutine that takes an integer as a argument
+    that it would be the max range for the random module, being
+    this value the delay until the value is returned
+    """
+    
     waiter = random.uniform(0, max_delay)
     await asyncio.sleep(waiter)
     return waiter
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -12,9 +12,9 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
-    A async routine called task_wait_n
+    An async coroutine called task_wait_n
     that takes in 2 int arguments: n and max_delay.
-    Will return a sorted list with
+    Will return the list of all the delays
     """
     tasks: List = []
     wait_time: List[float] = []
