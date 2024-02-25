aea0a5466cfce06023fd055d18f8dd2d8e46d708
Planisph3r3 <ennzopz@gmail.com>
2024-02-25 15:35:00 -0500

    Corrected annotation

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -9,7 +9,7 @@ import typing
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-def task_wait_random(max_delay: int = 10) -> asyncio.Task[float]:
+def task_wait_random(max_delay: int = 10) -> asyncio.Task:
     """A function that takes an integer max_delay and returns a asyncio.Task"""
-    task1: asyncio.Task[float] = asyncio.create_task(wait_random(max_delay))
+    task1: asyncio.Task = asyncio.create_task(wait_random(max_delay))
     return task1
