9ff9ff2dcf0e20f61b37bd0550192b8d82ed33b6
Planisph3r3 <ennzopz@gmail.com>
2024-03-24 18:52:09 -0500

    Creating a constructor on a class

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,7 +1,7 @@
 // ClassRoom Class
 
 export default class ClassRoom {
-    constructor (maxStudentsSize) {
-        this._maxStudentsSize = maxStudentsSize
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
