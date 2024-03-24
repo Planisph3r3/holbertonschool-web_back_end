c1b779b7d62b1ecd7df292d5438fa25665eb3cdd
Planisph3r3 <ennzopz@gmail.com>
2024-03-24 18:54:47 -0500

    Getter & Setters in ES6

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,59 @@
+// HolbertonCourse Class
+
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (Array.isArray(students)) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(lengthVal) {
+    if (typeof lengthVal === 'number') {
+      this._length = lengthVal;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(studentsVal) {
+    if (Array.isArray(studentsVal)) {
+      this._students = studentsVal;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+  }
+}
