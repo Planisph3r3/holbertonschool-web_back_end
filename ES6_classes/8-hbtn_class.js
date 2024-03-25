aba2bf7421c8404c031a117b36dcf661e6352131
Planisph3r3 <ennzopz@gmail.com>
2024-03-24 19:08:16 -0500

    String and Number representation when is casted

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+//  HolbertonClass Class
+
+export default class HolbertonClass {
+  constructor(size = 0, location = '') {
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
