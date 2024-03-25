81af9e7a70022bf94a19f3715c4d4cdd300abd41
Planisph3r3 <ennzopz@gmail.com>
2024-03-24 19:20:44 -0500

    Returning a new instance of a class calling a method

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,15 @@
+// Car Class Vroom
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const newCar = new Car();
+    Object.setPrototypeOf(newCar, Object.getPrototypeOf(this));
+    return newCar;
+  }
+}
