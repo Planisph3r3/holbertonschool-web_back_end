e81f8e43a8da74039abf8a7e5387ebb20ea8ee81
Planisph3r3 <ennzopz@gmail.com>
2024-04-14 16:48:30 -0500

    Fix: Correct instance of Error

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -2,7 +2,7 @@
 
 export default function uploadPhoto(filename) {
   const promise = new Promise((reject) => {
-    reject(Error(`${filename} cannot be processed`));
+    reject(new Error(`${filename} cannot be processed`));
   });
   return promise;
 }
