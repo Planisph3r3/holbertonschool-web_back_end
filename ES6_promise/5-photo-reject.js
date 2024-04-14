7d9ee87f0481c92ca7fdb16ea67829702c005f92
Planisph3r3 <ennzopz@gmail.com>
2024-04-14 16:41:47 -0500

    Simple Rejected Promise

---
+++ ES6_promise/5-photo-reject.js
@@ -0,0 +1,8 @@
+// Promises
+
+export default function uploadPhoto(filename) {
+  const promise = new Promise((reject) => {
+    reject(Error(`${filename} cannot be processed`));
+  });
+  return promise;
+}
