83d2c18141778456b919fa3d979d4e25ca61787e
Planisph3r3 <ennzopz@gmail.com>
2024-04-14 16:57:55 -0500

    Handle multiple promises

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import uploadPhoto from "./5-photo-reject";
+import signUpUser from "./4-user-promise";
+
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+      .then((results) => {
+        return results.map(result => ({
+          status: result.status,
+          value: result
+        }));
+      })
+  }
