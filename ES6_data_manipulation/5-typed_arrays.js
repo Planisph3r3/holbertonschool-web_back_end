b79530a97775bd245fb4e3e3eff4391043ca78dd
Planisph3r3 <ennzopz@gmail.com>
2024-04-21 16:25:40 -0500

    Fix: Safe if statement

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,9 @@
 // Typed Int
 
 export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
   const intArray = new ArrayBuffer(length);
   const view = new DataView(intArray, 0);
   view.setInt8(position, value);
