5e5639c303b54ee6b3c2f56707bb1d8db7c678b3
Planisph3r3 <ennzopz@gmail.com>
2024-04-21 16:15:37 -0500

    Fix: Using .map

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -4,9 +4,5 @@ export default function getListStudentIds(param) {
   if (!Array.isArray(param)) {
     return [];
   }
-  const arrayStore = [];
-  param.map((element) => {
-    arrayStore.push(element.id);
-  });
-  return arrayStore;
+  return param.map((element) => element.id);
 }
